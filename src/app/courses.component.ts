6913ef997d05d78af3ae4d4e54e85ae8add4c478
Stefan B <stefanbolcs@gmail.com>
2020-10-29 16:33:02 +0100

    installed bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,12 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', //<courses>
   template: `
-    <img [src]="imageUrl" />
-    <table>
-      <tr>
-        <td [attr.colspan]="colSpan"></td>
-      </tr>
-    </table>
+    <button class="btn btn-primary">SAVE</button>
   `,
 })
 export class CoursesComponent {
