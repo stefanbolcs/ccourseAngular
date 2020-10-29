e45ad38244ff3d3599a916da50ba1dbb73c69ca3
Stefan B <stefanbolcs@gmail.com>
2020-10-29 16:16:49 +0100

    attribute binding for image

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', //<courses>
   template: `<h2>{{ title }}</h2>
+  
+  <img [src]="imageUrl" />
     <ul>
       <li *ngFor="let course of courses">
         {{ course }}
@@ -14,6 +16,7 @@ import { Component } from '@angular/core';
 export class CoursesComponent {
   title = 'list of courses';
   courses=[];
+  imageUrl="https://picsum.photos/200/300"
 
   //constructor sees it has dependency, uses dependency injektion
   constructor(service: CoursesService) {
