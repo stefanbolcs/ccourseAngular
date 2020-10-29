7554ce8a735a061976bd7b531aa85ddc8f8712b6
Stefan B <stefanbolcs@gmail.com>
2020-10-29 16:36:37 +0100

    using bootstrap attribute classes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,10 +5,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', //<courses>
   template: `
-    <button class="btn btn-primary">SAVE</button>
+    <button class="btn btn-primary" [class.active]="isActive">SAVE</button>
   `,
 })
 export class CoursesComponent {
-  colSpan = 2;
-  imageUrl = 'https://picsum.photos/200/300';
+  isActive = true;
 }
