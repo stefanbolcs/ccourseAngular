a16c129c0df594e42ea54b52b37b6972d718a782
Stefan B <stefanbolcs@gmail.com>
2020-10-29 16:22:28 +0100

    binding to element attributes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,22 +4,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses', //<courses>
-  template: `<h2>{{ title }}</h2>
-  
-  <img [src]="imageUrl" />
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul> `,
+  template: `
+    <img [src]="imageUrl" />
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
+  `,
 })
 export class CoursesComponent {
-  title = 'list of courses';
-  courses=[];
-  imageUrl="https://picsum.photos/200/300"
-
-  //constructor sees it has dependency, uses dependency injektion
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
+  colSpan = 2;
+  imageUrl = 'https://picsum.photos/200/300';
 }
