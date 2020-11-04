81b71f532be96e0987bce8f509356e7171ee42ca
Stefan B <stefanbolcs@gmail.com>
2020-11-04 21:42:24 +0100

    2way binding with bananabox syntax ngModel

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,13 +5,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', //<courses>
   template: `
-  <input #email (keyup.enter)="onKeyUp(email.value)" />
+  <input [value]="email" (keyup.enter)="email=$event.target.value; onKeyUp()" />
+  <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+
   `,
 })
 export class CoursesComponent {
+  email="me@example.com";
 
-  onKeyUp(email){
-   console.log(email);
+  onKeyUp(){
+   console.log(this.email);
   }
   
 }
