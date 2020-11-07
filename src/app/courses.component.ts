7ed7ad8f66838c478f06fbd478c80fe8c2c946da
Stefan B <stefanbolcs@gmail.com>
2020-11-07 20:54:58 +0100

    angular pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,16 +5,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', //<courses>
   template: `
+  {{course.title | uppercase | lowercase}}<br />
+  {{course.students | number}}<br />
+  {{course.rating | number:'1.2-2'}}<br />
+  {{course.price | currency:'AUD':true:'3.2-2'}} <br />
+  {{course.releaseDate | date:'shortDate'}}
   
-  <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
 
   `,
 })
 export class CoursesComponent {
-  email="me@example.com";
-
-  onKeyUp(){
-   console.log(this.email);
+  //pipes
+  course = {
+    title:"The complete angualr course",
+    rating:4.9485,
+    students:33333,
+    price:196.47,
+    releaseDate: new Date(2018,1,1)
   }
   
 }
